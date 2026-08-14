1808096ad480bf31ceeae365c41676068e1d2efd
VictorAlexEtec2403427 <victor.gouveia@cps.sp.gov.br>
2026-08-14 13:54:20 -0300

    Fist Commit

---
+++ Aula001/index.php
@@ -0,0 +1,15 @@
+<?php 
+    echo "Hello World with php";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World with php</title>
+</head>
+<body>
+    
+</body>
+</html>
